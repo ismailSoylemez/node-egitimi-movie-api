849206b54fc1cdce1a6770dea2e0f1336e8d72fa
ismail <ismailsoylemez272@gmail.com>
2019-07-24 00:46:07 +0300

    index sayfası testinin yazılması

---
+++ test/index.test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+const server = require('../app');
+
+
+chai.use(chaiHttp);
+
+// testlerimizin ne testi olduğunu anlatıyoruz
+// açıklama giriyoruz
+// içerisinde birden fazla it() bulunabilir
+// it() aslında unit testtir
+describe('Node Server', () => {
+
+    it('(GET /) anasayfayı döndürür' , (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err,res) => {
+                res.should.have.status(200); //200 koduna sahip olmalı
+                done();
+            });
+
+
+    });
+
+
+
+});
